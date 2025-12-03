3d24b1bce336112fac55e7aab1d4b0b8b49a5a49
sandeepnambi <sandeep_nambi@srmap.edu.in>
2025-12-03 18:37:26 +0530

    Delete src directory

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-  timeout: 5000
-});
-
-export default api;
--- src/utils/toast.js
+++
@@ -1,5 +0,0 @@
-import { toast } from "react-toastify";
-
-export const notifySuccess = (msg) => toast.success(msg);
-export const notifyError = (msg) => toast.error(msg);
-export const notifyInfo = (msg) => toast.info(msg);
